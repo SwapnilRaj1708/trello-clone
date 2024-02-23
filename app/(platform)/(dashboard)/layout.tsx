92bec9405faf9dc3dda455a740f98d7972cf91e6
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-02-24 04:16:46 +0530

    feat: Add route protection to marketing and dashboard pages

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./_components/navbar";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full">
+      <Navbar /> {children}
+    </div>
+  );
+}
